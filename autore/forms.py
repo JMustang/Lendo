913389cb78c9673685cc3aae5100ddf539792f43
Junior <eecfredes@gmail.com>
2024-01-24 21:12:01 -0300

    :rocket: Criando form para adicionar os autores

---
+++ autore/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+
+from .models import Autor
+
+class AutorForm(ModelForm):
+    class Meta:
+        model = Autor
+        fields = ('name',)
--- autore/views.py
+++ autore/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
+from .forms import AutorForm
+
 def  index(request):
     return render(request, 'autor/index.html')
+
+def add(request):
+
+    form = AutorForm()
+    return render(request, 'autor/add.html', {'form': form})
