98919620f6bc59c137f1ed5a7db6cca9b05bcc61
Junior <eecfredes@gmail.com>
2024-01-25 21:33:40 -0300

    :rocket: Adicionando no DB

--- autore/views.py
+++ autore/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import AutorForm
 
-def  index(request):
+
+def index(request):
     return render(request, 'autor/index.html')
 
-def add(request):
 
-    form = AutorForm()
+def add(request):
+    if request.method == 'POST':
+        form = AutorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('autor:index')
+    else:
+        form = AutorForm()
     return render(request, 'autor/add.html', {'form': form})
