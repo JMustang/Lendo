fac09af5abf4138ad576f1171cf221a254051c4f
Junior <eecfredes@gmail.com>
2024-01-27 17:10:18 -0300

    :rocket: POST livros criado

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,6 @@ from django.urls import path,  include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('autor/', include('autore.urls')),
+    path('livros/', include('livros.urls')),
     path('', include('core.urls')),
 ]
--- livros/views.py
+++ livros/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import LivroForm
+
+def add(request):
+    if request.method =='POST':
+        form = LivroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('livros:index')
+    else:
+        form = LivroForm()
+
+    return render(request, 'livros/add.html',
+                  {'form': form})
+
